1a7b56f381d77b499dc6777ce678b2beb552e3b4
Markus Lachinger <1031928+mmlac@users.noreply.github.com>
2020-01-25 01:20:42 -0800

    Added simple install script

---
+++ install.zsh
@@ -0,0 +1,12 @@
+#!zsh
+
+#Where we at?
+FILEDIR=$(readlink --canonicalize $(dirname ${(%):-%x}))
+echo "I think we are in $FILEDIR"
+
+# Symlink everything in config subfolder to the ~/.config folder.
+# Skip if already present and don't alter
+
+for folder in config/*(/); do
+    [[ ! -a ~/.config/$folder ]] && ln -s $FILEDIR/config/$folder ~/.config/$folder
+done
