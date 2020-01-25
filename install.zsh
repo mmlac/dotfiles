b9c715fcd5470019a924fb9b9f7382af68b5745d
Markus Lachinger <1031928+mmlac@users.noreply.github.com>
2020-01-25 01:29:28 -0800

    create config folder if it does not exist first in install script

--- install.zsh
+++ install.zsh
@@ -7,6 +7,8 @@ echo "I think we are in $FILEDIR"
 # Symlink everything in config subfolder to the ~/.config folder.
 # Skip if already present and don't alter
 
+mkdir -p ~/.config
+
 for folder in config/*(/); do
     [[ ! -a ~/.config/$folder ]] && ln -s $FILEDIR/config/$folder ~/.config/$folder
 done
