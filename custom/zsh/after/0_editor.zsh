83b53ee1fa5711bf5e40db8b8ccfaa0c3ae662d0
Markus Lachinger <markus.lachinger@sap.com>
2020-08-06 17:36:15 -0700

    Git add credentials helper, editor use existing frame

--- custom/zsh/after/0_editor.zsh
+++ custom/zsh/after/0_editor.zsh
@@ -5,5 +5,9 @@ export EDITOR="emacsclient -t"                  # $EDITOR opens in terminal
 export VISUAL="emacsclient -c -a emacs"         # $VISUAL opens in GUI mode
 
 
-alias e="$EDITOR"
-alias em="$VISUAL"
+alias em="$EDITOR"
+unalias e
+
+function e() {
+    emacsclient -a "emacs" $@ &
+}
